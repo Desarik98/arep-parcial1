bc6d59737c2344617dedb070bfa41dcb09448dba
Desarik <daniel.mejia@mail.escuelaing.edu.co>
2021-09-09 12:00:37 -0500

    add trigonometric functions

--- src/main/java/edu/escuelaing/arep/HttpServer/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/HttpServer/HttpServer.java
@@ -87,25 +87,80 @@ public class HttpServer {
             type = "image/jpeg";
         }else if(path.endsWith(".png")){
             type = "image/png";
-        }else if(path.contains("?")){
-            System.out.println(path);
         }
-        Path file = Paths.get("./www" + path);
-        Charset charset = Charset.forName("UTF-8");
-        String outmsg = "";
-        try (BufferedReader reader = Files.newBufferedReader(file, charset)) {
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-                outmsg += "\r\n" + line;
+        if (path.startsWith("/calculadora?operation")){
+            String op = path.substring(23,26);
+            String num = path.substring(34);
+
+        }
+        else {
+            Path file = Paths.get("./www" + path);
+            Charset charset = Charset.forName("UTF-8");
+            String outmsg = "";
+            try (BufferedReader reader = Files.newBufferedReader(file, charset)) {
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                    outmsg += "\r\n" + line;
+                }
+            } catch (IOException x) {
+                System.err.format("IOException: %s%n", x);
             }
-        } catch (IOException x) {
-            System.err.format("IOException: %s%n", x);
+            return "HTTP/1.1 200 OK \r\n"
+                    + "Content-Type: "+type+"\r\n"
+                    + "\r\n"
+                    + outmsg;
         }
-        return "HTTP/1.1 200 OK \r\n"
-                + "Content-Type: "+type+"\r\n"
-                + "\r\n"
-                + outmsg;
+
+        public static String operations(String op,String input){
+            Double res = null;
+            double pi = 1;
+            String num = "";
+            String den = "";
+            System.out.println(op);
+            if (input.contains("π")){
+                pi = Math.PI;
+                input = input.substring(0,input.indexOf("π"))+input.substring(input.indexOf("π")+1);
+            }
+            if(input.contains("/")){
+                num = input.substring(0,input.indexOf("/"));
+                den = input.substring(input.indexOf("/")+1);
+                if(num.length() == 0){
+                    num = "1";
+                }
+                if(den.length() == 0){
+                    den = "1";
+                }
+            }
+            else {
+                den = "1";
+                if (input.length()==0){
+                    num = "1";
+                }else {
+                    num = input;
+                }
+            }
+            double r = Integer.parseInt(num)*pi/Integer.parseInt(den);
+            if(op == null){
+                op = "cos";
+            }
+            switch (op){
+                case "sin":
+                    System.out.println("Seno");
+                    res = Math.sin(r);
+                    break;
+                case "cos":
+                    System.out.println("Coseno");
+                    res = Math.cos(r);
+                    break;
+                case "tan":
+                    System.out.println("Tangente");
+                    res = Math.tan(r);
+                    break;
+            }
+            assert res != null;
+            return res.toString();
+        }
     }
 }
 
